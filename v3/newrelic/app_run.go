28a27b45de237214a074c97b8aac07e1a8e8d5f9
Rich Vanderwal <rvanderwal+githubpersonal@newrelic.com>
2021-09-15 16:36:51 -0700

    attempt worked, putting it back now

--- v3/newrelic/app_run.go
+++ v3/newrelic/app_run.go
@@ -106,7 +106,7 @@ func newAppRun(config config, reply *internal.ConnectReply) *appRun {
 
 	run.adaptiveSampler = newAdaptiveSampler(
 		time.Duration(reply.SamplingTargetPeriodInSeconds)*time.Second,
-		40,
+		reply.SamplingTarget,
 		time.Now())
 
 	if "" != run.Reply.RunID {
